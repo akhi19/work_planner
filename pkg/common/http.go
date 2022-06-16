50d7cae75a6645a02f9d732ec928f05c074c24f9
Akhilesh Jain <akhilesh@56secure.com>
2022-06-16 17:33:04 +0530

    Added http common file and validator

---
+++ pkg/common/http.go
@@ -0,0 +1,87 @@
+package common
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+)
+
+type Code int
+
+const (
+	serverErrorCode  Code = 500
+	paramMissingCode Code = 400
+)
+
+const (
+	InvalidRequestMsg = "Invalid Request"
+)
+
+type Entity struct {
+	HttpStatusCode int
+	Message        string
+	Code           Code
+}
+
+func (ue *Entity) Error() string {
+	return ue.Message
+}
+
+func ValidationError(msg string) error {
+	return &Entity{
+		HttpStatusCode: http.StatusBadRequest,
+		Message:        msg,
+		Code:           paramMissingCode,
+	}
+}
+
+func InternalServerError() error {
+	return &Entity{
+		HttpStatusCode: http.StatusInternalServerError,
+		Message:        "Something Went Wrong",
+		Code:           serverErrorCode,
+	}
+}
+
+func SendHttpError(ctx context.Context, w http.ResponseWriter, err error) {
+	httpStatusCode := http.StatusInternalServerError
+	code := serverErrorCode
+	message := err.Error()
+
+	if errVal, ok := err.(*Entity); ok {
+		httpStatusCode = errVal.HttpStatusCode
+		message = errVal.Message
+		code = errVal.Code
+	}
+
+	resp := map[string]interface{}{
+		"message": message,
+		"code":    code,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(httpStatusCode)
+	json.NewEncoder(w).Encode(resp)
+}
+
+func SendHttpResponse(
+	responseWriter http.ResponseWriter,
+	statusCode int,
+	response interface{},
+) {
+	responseWriter.Header().Set("Content-Type", "application/json")
+	responseWriter.WriteHeader(statusCode)
+	json.NewEncoder(responseWriter).Encode(response)
+}
+
+type emptyHttpResponse struct{}
+
+func SendEmptyHttpResponse(
+	responseWriter http.ResponseWriter,
+	statusCode int,
+) {
+	responseData := emptyHttpResponse{}
+	responseWriter.Header().Set("Content-Type", "application/json")
+	responseWriter.WriteHeader(statusCode)
+	json.NewEncoder(responseWriter).Encode(responseData)
+}
